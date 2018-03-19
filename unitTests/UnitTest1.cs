f2b0dafbe446cb26d1c14c178a9474d7893ae330
BartusZak <b.ploszynski@gmail.com>
2018-03-19 06:40:09 +0100

    Update.

--- unitTests/UnitTest1.cs
+++ unitTests/UnitTest1.cs
@@ -14,22 +14,20 @@ namespace unitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
-        {
-
-        }
-
-        [Fact]
         public void ShouldReturnListOfMovies()
         {
+            //arrange
             var movies = new List<Movie>() { new Movie(), new Movie() };
             var movieRepository = new Mock<IRepository<Movie>>();
+
+            //act
             movieRepository.Setup(x => x.GetAll()).Returns(movies);
             var movieService = new MovieService(movieRepository.Object);
             var movieController = new MovieController(movieService);
 
             var result = movieController.Get();
 
+            //assert
             Assert.Equal(movies, result);
         }
 
